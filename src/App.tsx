8140d959f37f2cc0d7b7660ba06ec3335cfcc8a7
Kristischik <kristinagi@mail.ru>
2023-05-29 17:09:52 +0300

    [lesson-38]:created input,textarea,username

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Button, {ButtonTypes} from "./components/Button";
 import Title from "./components/Title";
 import TabsList from './components/TabsList';
 import CardsList from "./components/CardsList";
+import Input from "./components/Input";
+import Username from "./components/Username";
 
 const App = () => {
-  return (
+
+    // Константы для Input
+    const [inputValue, setInputValue] = useState("");
+    const onChange = (value: string) => {
+        setInputValue(value);
+    };
+
+    return (
     <div>
       <Button
         type={ButtonTypes.Primary}
@@ -34,6 +43,16 @@ const App = () => {
         <TabsList />
 
         <CardsList />
+
+        <Input
+            isTextarea
+            title={"Test Input"}
+            placeholder={"Hello World!"}
+            onChange={onChange}
+            value={inputValue}
+        />
+
+        <Username username={"Kristina"} />
 
     </div>
   );
