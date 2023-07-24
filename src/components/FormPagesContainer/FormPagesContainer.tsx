61a417f7832e55bcb424541a7917b83f3d065ace
Kristischik <kristinagi@mail.ru>
2023-07-24 22:06:31 +0300

    [lesson 20] added ResetPasswordConfirmation

--- src/components/FormPagesContainer/FormPagesContainer.tsx
+++ src/components/FormPagesContainer/FormPagesContainer.tsx
@@ -3,45 +3,49 @@ import classNames from "classnames";
 import Title from "../Title";
 import styles from "./FormPagesContainer.module.scss";
 import Button, { ButtonTypes } from "../Button";
-import {useThemeContext} from "src/context/Theme";
-import {Theme} from "src/@types";
+import { useThemeContext } from "src/context/Theme";
+import { Theme } from "src/@types";
 
 type FormPagesContainerProps = {
-    title: string;
-    children: ReactElement | ReactElement[];
-    btnTitle: string;
-    onSubmit: () => void;
-    additionalInfo?: ReactElement;
+  title: string;
+  children: ReactElement | ReactElement[];
+  btnTitle: string;
+  onSubmit: () => void;
+  additionalInfo?: ReactElement;
+  isSubmitDisabled?: boolean;
 };
 
 const FormPagesContainer: FC<FormPagesContainerProps> = ({
-                                                             title,
-                                                             children,
-                                                             btnTitle,
-                                                             onSubmit,
-                                                             additionalInfo,
-                                                         }) => {
+  title,
+  children,
+  btnTitle,
+  onSubmit,
+  additionalInfo,
+  isSubmitDisabled,
+}) => {
+  const { themeValue } = useThemeContext();
 
-    const { themeValue } = useThemeContext();
-
-    return (
-        <div  className={classNames(styles.container, {
-            [styles.darkContainer]: themeValue === Theme.Dark,
-        })}>
-            <div className={styles.breadcrumbs}>Back to home</div>
-            <Title title={title} />
-            <div className={styles.formContainer}>
-                <div className={styles.fieldsContainer}>{children}</div>
-                <Button
-                    type={ButtonTypes.Primary}
-                    title={btnTitle}
-                    onClick={onSubmit}
-                    className={styles.button}
-                />
-                <div>{additionalInfo}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={classNames(styles.container, {
+        [styles.darkContainer]: themeValue === Theme.Dark,
+      })}
+    >
+      <div className={styles.breadcrumbs}>Back to home</div>
+      <Title title={title} />
+      <div className={styles.formContainer}>
+        <div className={styles.fieldsContainer}>{children}</div>
+        <Button
+          type={ButtonTypes.Primary}
+          title={btnTitle}
+          onClick={onSubmit}
+          className={styles.button}
+          disabled={isSubmitDisabled}
+        />
+        <div>{additionalInfo}</div>
+      </div>
+    </div>
+  );
 };
 
 export default FormPagesContainer;
--- src/pages/ResetPasswordConfirmation/ResetPasswordConfirmation.tsx
+++ src/pages/ResetPasswordConfirmation/ResetPasswordConfirmation.tsx
@@ -1,14 +1,17 @@
-import React, {useState} from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import FormPagesContainer from "src/components/FormPagesContainer";
 import classNames from "classnames";
 
-import {Theme} from "src/@types";
-import {useThemeContext} from "src/context/Theme";
+import { Theme } from "src/@types";
+import { useThemeContext } from "src/context/Theme";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import {activateUser, resetPasswordConfirm} from "src/redux/reducers/authSlice";
-import {RoutesList} from "src/pages/Router";
+import {
+  activateUser,
+  resetPasswordConfirm,
+} from "src/redux/reducers/authSlice";
+import { RoutesList } from "src/pages/Router";
 import Input from "src/components/Input";
 
 const ResetPasswordConfirmation = () => {
@@ -19,37 +22,84 @@ const ResetPasswordConfirmation = () => {
   const dispatch = useDispatch();
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const [isTouched, setTouched] = useState({
+    passwordField: false,
+    confirm: false,
+  });
+
   const onSubmit = () => {
     if (uid && token) {
       dispatch(
         resetPasswordConfirm({
           data: { uid, token, new_password: password },
           callback: () => {
-            navigate(RoutesList.SignIn)
+            navigate(RoutesList.SignIn);
           },
         })
       );
     }
   };
 
+  const onChangePassword = (value: string) => {
+    setPassword(value);
+    setTouched((prevState) => ({ ...prevState, passwordField: true }));
+  };
+  const onChangePasswordConfirm = (value: string) => {
+    setConfirmPassword(value);
+    setTouched((prevState) => ({ ...prevState, confirm: true }));
+  };
+
+  useEffect(() => {
+    if (isTouched.passwordField && isTouched.confirm) {
+      if (password !== confirmPassword) {
+        setPasswordError("Passwords Must Match");
+      } else if (password.length < 8 || confirmPassword.length < 8) {
+        setPasswordError("Passwords length should be more than 8 characters");
+      } else {
+        setPasswordError("");
+      }
+    }
+  }, [password, confirmPassword, isTouched.passwordField, isTouched.confirm]);
+
+  const isSubmitValid = useMemo(
+    () =>
+      isTouched.passwordField &&
+      isTouched.confirm &&
+      password.length &&
+      confirmPassword.length &&
+      !passwordError.length,
+    [
+      password,
+      confirmPassword,
+      isTouched.passwordField,
+      isTouched.confirm,
+      passwordError,
+    ]
+  );
+
   return (
     <FormPagesContainer
       title={"New Password"}
       btnTitle={"Set Password"}
+      isSubmitDisabled={!isSubmitValid}
       onSubmit={onSubmit}
     >
       <div>
         <Input
           title={"Password"}
           placeholder={"Your password"}
-          onChange={setPassword}
+          onChange={onChangePassword}
           value={password}
+          errorText={passwordError}
         />
         <Input
           title={"Confirm Password"}
           placeholder={"Confirm password"}
-          onChange={setConfirmPassword}
+          onChange={onChangePasswordConfirm}
           value={confirmPassword}
+          errorText={passwordError}
         />
       </div>
     </FormPagesContainer>
